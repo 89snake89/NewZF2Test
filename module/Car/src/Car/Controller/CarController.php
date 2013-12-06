f0f660969db4ddbcff2bc47f256ff44f6e670440
89snake89 <bottanfabio@hotmail.it>
2013-12-06 15:15:25 +0100

    Add action completed

--- module/Car/src/Car/Controller/CarController.php
+++ module/Car/src/Car/Controller/CarController.php
@@ -4,6 +4,7 @@ namespace Car\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Car\Model\Car;
+use Car\Form\CarForm;
 
 class CarController extends AbstractActionController
 {
@@ -18,7 +19,26 @@ class CarController extends AbstractActionController
 	}
 	
 	public function addAction(){
+		$carForm = new CarForm();
+		$carForm->get('submit')->setValue('Add');
 		
+		$request = $this->getRequest();
+		
+		if($request->isPost()){
+			$car = new Car();
+			$car->setInputFilter($car->getInputFilter());
+			$carForm->setData($request->getPost());
+			
+			if($carForm->isValid()){
+				$car->exchangeArray($carForm->getData());
+				$this->getCarTable()->saveCar();
+				
+				// Redirect to list of albums
+				return $this->redirect()->toRoute('car');
+			}
+		}
+		
+		return array('form' => $carForm);
 	}
 	
 	public function editAction(){
