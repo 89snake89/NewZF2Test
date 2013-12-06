e6b43297f493d5b4352c6b36a1223f138527c4cb
89snake89 <bottanfabio@hotmail.it>
2013-12-06 11:57:40 +0100

    Add "AddAction" on controller

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -3,6 +3,8 @@ namespace Album\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Album\Model\Album;
+use Album\Form\AlbumForm;
 
 class AlbumController extends AbstractActionController
 {
@@ -17,6 +19,25 @@ class AlbumController extends AbstractActionController
 
 	public function addAction()
 	{
+		$form = new AlbumForm();
+		$form->get('submit')->setValue('Add');
+		
+		$request = $this->getRequest();
+		if ($request->isPost()) {
+			$album = new Album();
+			$form->setInputFilter($album->getInputFilter());
+			$form->setData($request->getPost());
+		
+			if ($form->isValid()) {
+				$album->exchangeArray($form->getData());
+				$this->getAlbumTable()->saveAlbum($album);
+		
+				// Redirect to list of albums
+				return $this->redirect()->toRoute('album');
+			}
+		}
+		return array('form' => $form);
+		
 	}
 
 	public function editAction()
